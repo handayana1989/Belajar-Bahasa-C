5d15d7e6369616f20cf12ea87649c3e483a419a3
I Gusti Ngurah Yudi Handayana <68172336+handayana1989@users.noreply.github.com>
2020-07-12 09:54:09 +0800

    Upload belajar C hello world
    Upload hello world

---
+++ hello01.c
@@ -0,0 +1,15 @@
+/*
+PROGRAM : HELLO WORLD
+DATE    : 25 Juni 2020
+OLEH	: I Gusti Ngurah Yudi Handayana
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello C World");
+	
+system("PAUSE");
+return 0;
+}
