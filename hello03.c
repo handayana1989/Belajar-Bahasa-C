f3067e90cbab236245d4a1e392e1efa374b65179
I Gusti Ngurah Yudi Handayana <68172336+handayana1989@users.noreply.github.com>
2020-07-12 10:41:41 +0800

    kurangi print

--- hello03.c
+++ hello03.c
@@ -8,7 +8,6 @@ OLEH	: I Gusti Ngurah Yudi Handayana
 
 int main(int argc, char *argv[])
 {
-	printf("tab a\t b \t c");
 	printf("baris baru a\n b \n c\n");
 	printf("garis miring \\ tanda kutip \" atau \'");
 	printf("persen %%");
