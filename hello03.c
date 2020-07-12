450a2daddc125847b19bd982499b4a03feefb8be
I Gusti Ngurah Yudi Handayana <68172336+handayana1989@users.noreply.github.com>
2020-07-12 10:39:13 +0800

    menambah baris baru atau fungsi lainnya

---
+++ hello03.c
@@ -0,0 +1,17 @@
+/*
+PROGRAM : HELLO WORLD
+DATE    : 25 Juni 2020
+OLEH	: I Gusti Ngurah Yudi Handayana
+*/
+
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("tab a\t b \t c");
+	printf("baris baru a\n b \n c\n");
+	printf("garis miring \\ tanda kutip \" atau \'");
+	printf("persen %%");
+system("PAUSE");
+return 0 ;
+}
